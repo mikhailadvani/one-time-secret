fb27621025e14461b6e01c90d368fd2d4cf21662
Mikhail Advani <mikhail.advani@gmail.com>
2019-09-08 18:56:56 +0200

    Minimal UI

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -27,11 +28,41 @@ type GetSecretResponse struct {
 
 // Index will return the welcome screen. Static HTML page from S3
 func Index() string {
-	return "Welcome!"
+	return `
+	<!DOCTYPE html>
+	<html>
+	<body>
+
+	<div style="width:400px; margin:0 auto;display:flex;justify-content:center;" id="container">
+		<div id="create">
+			<h2>Enter your secret</h2>
+		  <input id="input_secret"></input>
+		  <button type="button" onclick="create()">Submit</button>
+		</div>
+		<div>
+			<p id="generated_secret_id"></p>
+		</div>
+	</div>
+	</body>
+	<script>
+	  function create() {
+	    var xhttp = new XMLHttpRequest();
+			var secret_text = document.getElementById("input_secret").value;
+	    xhttp.onreadystatechange = function() {
+	      if (this.readyState == 4 && this.status == 200) {
+	       document.getElementById("generated_secret_id").innerHTML = JSON.parse(this.responseText).url;
+	      }
+	    };
+	    xhttp.open("POST", "/api/v1/secret", true);
+	    xhttp.send(` + "`" + `{"content": "${secret_text}"}` + "`" + `);
+	  }
+	</script>
+	</html>
+	`
 }
 
 // CreateSecret will create a secret object an return a URL to access it by
-func CreateSecret(requestBody io.Reader) (CreateSecretResponse, error) {
+func CreateSecret(requestBody io.Reader, prefix string) (CreateSecretResponse, error) {
 	decoder := json.NewDecoder(requestBody)
 	var request CreateSecretRequest
 	err := decoder.Decode(&request)
@@ -42,7 +73,7 @@ func CreateSecret(requestBody io.Reader) (CreateSecretResponse, error) {
 	if err != nil {
 		return CreateSecretResponse{Status: http.StatusInternalServerError}, err
 	}
-	secretResponse := CreateSecretResponse{URL: secretLocation, Status: http.StatusOK}
+	secretResponse := CreateSecretResponse{URL: fmt.Sprintf("%s/%s", prefix, secretLocation), Status: http.StatusOK}
 	return secretResponse, nil
 }
 
--- pkg/http/http.go
+++ pkg/http/http.go
@@ -10,13 +10,17 @@ import (
 	"github.com/mikhailadvani/one-time-secret/pkg/api"
 )
 
+var getAPIBase = "/api/v1/secret"
+var getAPI = fmt.Sprintf("%s/{secretID}", getAPIBase)
+var createAPI = "/api/v1/secret"
+
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, api.Index())
 }
 
 func createSecret(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	secret, err := api.CreateSecret(r.Body)
+	secret, err := api.CreateSecret(r.Body, r.Host+getAPIBase)
 	if err != nil {
 		http.Error(w, "{}", secret.Status)
 	}
@@ -37,8 +41,8 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index).Methods("GET")
-	router.HandleFunc("/api/v1/secret", createSecret).Methods("POST")
-	router.HandleFunc("/api/v1/secret/{secretID}", getSecret).Methods("GET")
+	router.HandleFunc(createAPI, createSecret).Methods("POST")
+	router.HandleFunc(getAPI, getSecret).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
