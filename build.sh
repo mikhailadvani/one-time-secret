7b53aee635cb46eac44175e04450393af9ed6bd2
Mikhail Advani <mikhail.advani@gmail.com>
2019-09-21 01:54:41 +0200

    Build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+set -e
+
+rm -rf ./build
+mkdir -p ./build
+go get ./pkg/*
+go test ./pkg/*
+GOOS=linux go build -o ./build/one-time-secret-http-linux pkg/http/http.go
+GOOS=darwin go build -o ./build/one-time-secret-http-darwin pkg/http/http.go
+
+GOOS=linux GOARCH=amd64 go build -o ./build/one-time-secret-index pkg/lambda-index/main.go
+GOOS=linux GOARCH=amd64 go build -o ./build/one-time-secret-create pkg/lambda-create/main.go
+
+zip -j ./build/one-time-secret-index.zip ./build/one-time-secret-index
+zip -j ./build/one-time-secret-create.zip ./build/one-time-secret-create
